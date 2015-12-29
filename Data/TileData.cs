76581d411e07c3bdf9bb341c237126e5c2dd86a3
Phil Oats <phillip.oats@gmail.com>
2015-12-29 21:21:09 +0000

    Create class

---
+++ Data/TileData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileData : MonoBehaviour {
+
+    public string blockType = "basic_cube";
+    public bool breakable;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
