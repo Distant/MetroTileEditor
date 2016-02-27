5bd8eb4bba37b56f5d2058a37981b39ad9cdabb0
Phil Oats <phillip.oats@gmail.com>
2016-02-27 14:20:00 +0000

    Update to use Index

--- Data/MapSaveData.cs
+++ Data/MapSaveData.cs
@@ -8,7 +8,7 @@ namespace MetroTileEditor
     public class MapSaveData
     {
         public BlockData[] blockData1D;
-        public int x, y, z;
+        public int width, height, depth;
         public int selectedLayer;
 
         public MapSaveData(BlockDataArray blockDataArray, int layer)
@@ -17,33 +17,33 @@ namespace MetroTileEditor
             selectedLayer = layer;
         }
 
-        public MapSaveData(BlockDataArray blockDataArray, int x, int y, int z, int layer)
+        public MapSaveData(BlockDataArray blockDataArray, Index size, int layer)
         {
             blockData1D = blockDataArray.Data;
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.width = size.x;
+            this.height = size.y;
+            this.depth = size.z;
             selectedLayer = layer;
         }
 
-        public MapSaveData(BlockDataArray blockDataArray, int x, int y, int z)
+        public MapSaveData(BlockDataArray blockDataArray, Index size)
         {
             blockData1D = blockDataArray.Data;
 
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.width = size.x;
+            this.height = size.y;
+            this.depth = size.z;
             selectedLayer = 0;
         }
 
         public static MapSaveData Empty()
         {
-            return new MapSaveData(new BlockDataArray(1,1,1), 0);
+            return new MapSaveData(new BlockDataArray(1, 1, 1), 0);
         }
 
         public BlockDataArray Get3DData()
         {
-            BlockDataArray blockData = new BlockDataArray(x, y, z);
+            BlockDataArray blockData = new BlockDataArray(width, height, depth);
             blockData.Data = blockData1D;
             return blockData;
         }
