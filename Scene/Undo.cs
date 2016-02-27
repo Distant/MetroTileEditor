115a02b68252c3c8f9748d5b3df755ff0520c4c6
Phil Oats <phillip.oats@gmail.com>
2016-02-27 14:18:37 +0000

    Remove premade commands

--- Scene/Undo.cs
+++ Scene/Undo.cs
@@ -2,113 +2,59 @@
 using System.Collections.Generic;
 using MetroTileEditor;
 using System;
-
-public class Undo : ISerializationCallbackReceiver
+namespace MetroTileEditor
 {
-
-    public interface UndoCommand
-    {
-        void Undo();
-        void Redo();
-    }
-
-    private Stack<UndoCommand> undoStack;
-    private Stack<UndoCommand> redoStack;
-
-    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    public class Undo
     {
 
-    }
-
-    void ISerializationCallbackReceiver.OnAfterDeserialize()
-    {
-
-    }
-
-    public Undo()
-    {
-        undoStack = new Stack<UndoCommand>();
-        redoStack = new Stack<UndoCommand>();
-    }
-
-    public void PerformUndo()
-    {
-        if (undoStack.Count > 0)
+        public class UndoCommand
         {
-            UndoCommand command = undoStack.Pop();
-            command.Undo();
-            redoStack.Push(command);
+            public Action Undo;
+            public Action Redo;
+
+            public UndoCommand(Action Undo, Action Redo)
+            {
+                this.Undo = Undo;
+                this.Redo = Redo;
+            }
+
+
         }
-    }
 
-    public void PerformRedo()
-    {
-        if (redoStack.Count > 0)
+        private readonly int MAX_UNDO = 50;
+        private Stack<UndoCommand> undoStack;
+        private Stack<UndoCommand> redoStack;
+
+        public Undo()
         {
-            UndoCommand command = redoStack.Pop();
-            command.Redo();
+            undoStack = new Stack<UndoCommand>();
+            redoStack = new Stack<UndoCommand>();
+        }
+
+        public void RegisterUndo(Undo.UndoCommand command)
+        {
             undoStack.Push(command);
-        }
-    }
-
-    public void RegisterUndo(UndoCommand command)
-    {
-        undoStack.Push(command);
-        redoStack.Clear();
-    }
-
-    public class AddCommand : UndoCommand
-    {
-        int x, y, z;
-        string blockType;
-        MapObject obj;
-
-        public AddCommand(int x, int y, int z, string blockType, MapObject obj)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.blockType = blockType;
-            this.obj = obj;
+            redoStack.Clear();
         }
 
-        public void Redo()
+        public void PerformUndo()
         {
-            obj._AddBlock(x, y, z, blockType);
+            if (undoStack.Count > 0)
+            {
+                UndoCommand command = undoStack.Pop();
+                command.Undo();
+                redoStack.Push(command);
+            }
         }
 
-        public void Undo()
+        public void PerformRedo()
         {
-            obj.blocks.DeleteBlock(x, y, z);
-            obj.DrawBlocks();
-        }
-    }
-
-    public class DeleteCommand : UndoCommand
-    {
-        int x, y, z;
-        string blockType;
-        MapObject obj;
-        BlockData data;
-
-        public DeleteCommand(int x, int y, int z, BlockData data, MapObject obj)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.data = data;
-            this.obj = obj;
-        }
-
-        public void Redo()
-        {
-            obj.blocks.DeleteBlock(x, y, z);
-            obj.DrawBlocks();
-        }
-
-        public void Undo()
-        {
-            obj._AddBlock(x, y, z, data);
+            if (redoStack.Count > 0)
+            {
+                UndoCommand command = redoStack.Pop();
+                command.Redo();
+                undoStack.Push(command);
+            }
         }
     }
 }
